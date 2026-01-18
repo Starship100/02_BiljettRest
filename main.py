19dd756456a7009deedd0a653aa9561bcfd24c72
Starship100 <jimmy.widlund@outlook.com>
2026-01-18 01:43:24 +0100

    First

---
+++ main.py
@@ -0,0 +1,8 @@
+
+
+ticket_price = 100
+account = 200
+sum = account - ticket_price
+print("Det blir " + str(account - ticket_price) + " kronor över.")
+Split = int(sum / 2)
+print("Varje person får " + str(Split) + " kronor.")
